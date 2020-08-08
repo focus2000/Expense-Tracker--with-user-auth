c7370489b4dbd05e51443722608a46286b1d7bfa
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-08-08 09:38:09 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
@@ -25,10 +26,18 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1/transactions', transactions);
-app.use('/users',  require('./routes/user'))
+app.use('/users',  require('./routes/user'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 
 
 const PORT = process.env.PORT || 5000;
+
 
 
 app.listen(PORT, console.log(`server running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
