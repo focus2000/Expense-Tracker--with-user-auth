5b58ca89be475be985982764aeb91c6fbdd0f567
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-08-15 12:39:35 +0100

    dev dev

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
+require("dotenv").config()
 const colors = require('colors');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 
-dotenv.config({path:'./config/config.env'});
 
 connectDB();
 
@@ -30,7 +29,6 @@ app.use('/users',  require('./routes/user'));
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-
     app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
