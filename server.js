a749a0bdf1fa599f310c582f8fe4c56357e3b160
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-08-15 12:53:10 +0100

    git

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use('/api/v1/transactions', transactions);
 app.use('/users',  require('./routes/user'));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.use(express.static('clients/build'));
+    app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, 'clients', 'build', 'index.html')));
 }
 
 
